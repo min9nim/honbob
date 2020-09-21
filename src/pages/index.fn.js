043a45d6fefad789dbe9ec63dfa78753571c9014
min9nim <min9nim@gmail.com>
2020-09-21 15:36:42 +0900

    fix Mixed content error

--- src/pages/index.fn.js
+++ src/pages/index.fn.js
@@ -4,7 +4,7 @@ export const getLocations = async (list) => {
   const locations = await Promise.all(
     list.map(user =>
       req.get(
-        `http://api.ipstack.com/${user.ip}?access_key=4448b8b2be662b3927372b0c185b30d7&format=1`,
+        `//api.ipstack.com/${user.ip}?access_key=4448b8b2be662b3927372b0c185b30d7&format=1`,
       ),
     ),
   )
