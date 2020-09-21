a2f1a32dd901d60ba8c8b0133c31876bd1381828
min9nim <min9nim@gmail.com>
2020-09-22 07:17:34 +0900

    refactor

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import React from 'react'
 import Loading from './components/Loading'
 import Routes from './Routes'
 import { LoadingProvider } from 'react-hook-loading'
+import h, { div } from 'utils/hyperscript'
 import 'antd/dist/antd.css'
 import './App.scss'
 
 export default function App() {
-  return <div className="app">
-    <LoadingProvider loading={<Loading />}>
-      <Routes />
-    </LoadingProvider>
-  </div>
+  return div('.app', h(LoadingProvider, { loading: h(Loading) }, h(Routes)))
 }
