e1a1bdf8a8c1344255b6bd9d8b956de3ed074a4f
min9nim <min9nim@gmail.com>
2020-09-22 07:19:42 +0900

    fix type error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from 'react'
 import Loading from './components/Loading'
 import Routes from './Routes'
 import { LoadingProvider } from 'react-hook-loading'
-import h, { div } from 'utils/hyperscript'
+import h, { div } from './utils/hyperscript'
 import 'antd/dist/antd.css'
 import './App.scss'
 
-export default function App() {
-  return div('.app', h(LoadingProvider, { loading: h(Loading) }, h(Routes)))
+export default () => {
+  return div('.app', [h(LoadingProvider, { loading: h(Loading) }, h(Routes))])
 }
