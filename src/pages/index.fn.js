52fb24249f248ba910d2eb1b3fe66f485e606a3c
min9nim <min9nim@gmail.com>
2020-09-21 15:32:36 +0900

    new getLocations

---
+++ src/pages/index.fn.js
@@ -0,0 +1,12 @@
+import req from '../utils/req'
+
+export const getLocations = async (list) => {
+  const locations = await Promise.all(
+    list.map(user =>
+      req.get(
+        `http://api.ipstack.com/${user.ip}?access_key=4448b8b2be662b3927372b0c185b30d7&format=1`,
+      ),
+    ),
+  )
+  return locations
+}
