ce110b9adf0542ccece1e2bddf66bf54926c9802
min9nim <min9nim@gmail.com>
2020-09-22 07:06:44 +0900

    miniurl 흔적 지우기

--- src/App.js
+++ src/App.js
@@ -1,35 +1,14 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Loading from './components/Loading'
 import Routes from './Routes'
 import { LoadingProvider } from 'react-hook-loading'
 import 'antd/dist/antd.css'
-import shortid from 'shortid'
-import req from './utils/req'
 import './App.scss'
 
 export default function App() {
-  const [state, setState] = useState('Loading..')
-  const miniUrlId = window.location.pathname.slice(1)
-  const forward = shortid.isValid(miniUrlId)
-  window.$logger.debug({forward})
-
-  useEffect(() => {
-    if (!forward) {
-      return
-    }
-    req('/api/url?miniUrlId=' + miniUrlId).then(({ originUrl }) => {
-      if (originUrl) {
-        window.location.href = originUrl
-      }else{
-        setState('Not found URL 😱')
-      }
-    })
-  }, [])
-  return forward ? state : (
-    <div className="app">
-      <LoadingProvider loading={<Loading />}>
-        <Routes />
-      </LoadingProvider>
-    </div>
-  )
+  return <div className="app">
+    <LoadingProvider loading={<Loading />}>
+      <Routes />
+    </LoadingProvider>
+  </div>
 }
